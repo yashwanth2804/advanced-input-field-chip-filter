98ce1f67fb833497dcc3898df2e67450e287acd2
kambala yashwanth <kambaalayashwanth@outlook.com>
2024-11-05 19:48:43 +0000

    added github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import FilterComp from "./FilterComp";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen flex-col">
       <FilterComp />
+      <div className="mt-12 ">
+        <a href="https://github.com/yashwanth2804/inline-string-filtering-code" className="text-3xl font-bold hover:text-blue-400"> Github </a>
+      </div>
     </div>
   );
 }
